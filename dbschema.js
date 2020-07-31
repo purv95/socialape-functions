3725e6b4d04a51615aefdbe4acfd4974e3341fb3
purv1911 <36823475+purv1911@users.noreply.github.com>
2020-07-31 16:27:27 +0200

    Initial commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-07-19T21:20:22.445Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
